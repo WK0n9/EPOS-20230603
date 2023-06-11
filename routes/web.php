b241012d9fb0ea2a685b929076b8ee1ba9451a63
WK0n9 <veneto@tute.edu.cn>
2023-06-11 10:22:43 +0800

    2023-06-10 修改了地址和路径的兼容性相关，以便在同一个wamp64服务器下运行多个项目(懒得改端口了捏，直接给俩项目扔一个文件夹里)

--- routes/web.php
+++ routes/web.php
@@ -14,39 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix("/epos/")->group(function (){
     Route::get('/home', function () {
-        return view('welcome');
+        return view('epos_welcome');
     });
-    Route::get('/', [\App\Http\Controllers\IndexController::class,"index"])->name("home");//主页，选择桌号
-    Route::get('/order', [\App\Http\Controllers\IndexController::class,"order"])->name("order");//点餐
-    Route::get('/order_again', [\App\Http\Controllers\IndexController::class,"order_again"])->name("order_again");//加单
-    Route::get('/purchase', [\App\Http\Controllers\IndexController::class,"purchase"])->name("purchase");//点餐
-    Route::get('/bill', [\App\Http\Controllers\IndexController::class,"bill"])->name("bill");//点餐
-    Route::get('/personal', [\App\Http\Controllers\IndexController::class,"personal"])->name("personal");//点餐
+    Route::get('/', [\App\Http\Controllers\EposController::class,"index"])->name("epos_home");//主页，选择桌号
+    Route::get('/order', [\App\Http\Controllers\EposController::class,"order"])->name("epos_order");//点餐
+    Route::get('/order_again', [\App\Http\Controllers\EposController::class,"order_again"])->name("epos_order_again");//加单
+    Route::get('/purchase', [\App\Http\Controllers\EposController::class,"purchase"])->name("epos_purchase");//点餐
+    Route::get('/bill', [\App\Http\Controllers\EposController::class,"bill"])->name("epos_bill");//点餐
+    Route::get('/personal', [\App\Http\Controllers\EposController::class,"personal"])->name("epos_personal");//点餐
 
-    Route::post('/get_index', [\App\Http\Controllers\IndexController::class,"get_index"]);//获取主页所需信息
-    Route::post('/get_order', [\App\Http\Controllers\IndexController::class,"get_order"]);//获取点餐所需信息
-    Route::post('/get_cate', [\App\Http\Controllers\IndexController::class,"get_cate"]);//获取菜品分类信息
-    Route::post('/get_dish', [\App\Http\Controllers\IndexController::class,"get_dish"]);//获取菜品所需信息
-    Route::post('/get_item', [\App\Http\Controllers\IndexController::class,"get_item"]);//获取库存所需信息
-    Route::post('/get_bill_equal', [\App\Http\Controllers\IndexController::class,"get_bill_equal"]);//获取账单所需信息
-    Route::post('/get_ori_order', [\App\Http\Controllers\IndexController::class,"get_ori_order"]);//加单时获取已点信息
-    Route::post('/get_finish', [\App\Http\Controllers\IndexController::class,"get_finish"]);//获取结单信息
+    Route::post('/get_index', [\App\Http\Controllers\EposController::class,"get_index"]);//获取主页所需信息
+    Route::post('/get_order', [\App\Http\Controllers\EposController::class,"get_order"]);//获取点餐所需信息
+    Route::post('/get_cate', [\App\Http\Controllers\EposController::class,"get_cate"]);//获取菜品分类信息
+    Route::post('/get_dish', [\App\Http\Controllers\EposController::class,"get_dish"]);//获取菜品所需信息
+    Route::post('/get_item', [\App\Http\Controllers\EposController::class,"get_item"]);//获取库存所需信息
+    Route::post('/get_bill_equal', [\App\Http\Controllers\EposController::class,"get_bill_equal"]);//获取账单所需信息
+    Route::post('/get_ori_order', [\App\Http\Controllers\EposController::class,"get_ori_order"]);//加单时获取已点信息
+    Route::post('/get_finish', [\App\Http\Controllers\EposController::class,"get_finish"]);//获取结单信息
 
-    Route::post('/add_order', [\App\Http\Controllers\IndexController::class,"add_order"]);//提交点单
-    Route::post('/add_new_order', [\App\Http\Controllers\IndexController::class,"add_new_order"]);//提交加单
-    Route::post('/add_dish', [\App\Http\Controllers\IndexController::class,"add_dish"]);//添加菜品
-    Route::post('/add_item', [\App\Http\Controllers\IndexController::class,"add_item"]);//添加菜品
-    Route::post('/add_finish', [\App\Http\Controllers\IndexController::class,"add_finish"]);//提交结单
+    Route::post('/add_order', [\App\Http\Controllers\EposController::class,"add_order"]);//提交点单
+    Route::post('/add_new_order', [\App\Http\Controllers\EposController::class,"add_new_order"]);//提交加单
+    Route::post('/add_dish', [\App\Http\Controllers\EposController::class,"add_dish"]);//添加菜品
+    Route::post('/add_item', [\App\Http\Controllers\EposController::class,"add_item"]);//添加菜品
+    Route::post('/add_finish', [\App\Http\Controllers\EposController::class,"add_finish"]);//提交结单
 
-    Route::post('/edit_dish', [\App\Http\Controllers\IndexController::class,"edit_dish"]);//修改菜品
-    Route::post('/edit_item', [\App\Http\Controllers\IndexController::class,"edit_item"]);//修改货物
+    Route::post('/edit_dish', [\App\Http\Controllers\EposController::class,"edit_dish"]);//修改菜品
+    Route::post('/edit_item', [\App\Http\Controllers\EposController::class,"edit_item"]);//修改货物
 
-    Route::post('/delete_dish', [\App\Http\Controllers\IndexController::class,"delete_dish"]);//删除菜品
-    Route::post('/delete_item', [\App\Http\Controllers\IndexController::class,"delete_item"]);//删除货物
-    Route::post('/delete_bill', [\App\Http\Controllers\IndexController::class,"delete_bill"]);//删除账单
+    Route::post('/delete_dish', [\App\Http\Controllers\EposController::class,"delete_dish"]);//删除菜品
+    Route::post('/delete_item', [\App\Http\Controllers\EposController::class,"delete_item"]);//删除货物
+    Route::post('/delete_bill', [\App\Http\Controllers\EposController::class,"delete_bill"]);//删除账单
 
-    Route::post('/calc_income', [\App\Http\Controllers\IndexController::class,"calc_income"]);//查询应收实收
-    Route::post('/calc_cost', [\App\Http\Controllers\IndexController::class,"calc_cost"]);//查询菜品成本
-    Route::post('/calc_item', [\App\Http\Controllers\IndexController::class,"calc_item"]);//查询菜品成本
+    Route::post('/calc_income', [\App\Http\Controllers\EposController::class,"calc_income"]);//查询应收实收
+    Route::post('/calc_cost', [\App\Http\Controllers\EposController::class,"calc_cost"]);//查询菜品成本
+    Route::post('/calc_item', [\App\Http\Controllers\EposController::class,"calc_item"]);//查询菜品成本
 });
 
